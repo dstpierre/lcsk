4afd0eda4ee8ff5c51c0977e2f78398c063f89f1
dstpierre <none@none>
2007-07-12 00:29:10 +0000

    Send button now uses AJAX
    --HG--
    extra : convert_revision : svn%3A11aa8812-6de0-41ed-92bd-ec94540cc92c%4024577

--- website/Chat.aspx
+++ website/Chat.aspx
@@ -1,8 +1,7 @@
 <%@ Page Language="C#" AutoEventWireup="true" CodeFile="Chat.aspx.cs" Inherits="Chat" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" >
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Chat Session</title>
     <style type="text/css">
@@ -15,7 +14,7 @@
     #Welcome
     {
 		font-weight: bold;
-		color: blue;
+		color: gray;
 		font-size: 12pt;
 		width: 500px;
     }
@@ -30,9 +29,9 @@
 		overflow: auto;
 		padding: 8px;
 	}
-	.chatName { color: blue; }
-    </style>
-    <script language="javascript">
+	.chatName { color: gray; }
+    </style>  
+        <script language="javascript" type="text/javascript">
 
 		function scrollDiv()
 		{
@@ -48,52 +47,66 @@
 		
 		window.onload = scrollDiv;
     </script>
+
 </head>
 <body topmargin="0" leftmargin="0">
     <form id="formMain" runat="server">
-    <asp:ScriptManager ID="ScriptManager1" runat="server" />
-    <img src="Images/logo_base.jpg" alt="Place your logo here" /><br /><br />
-    <asp:panel id="pnlNoOperator" visible="false" runat="server">
-		<div style="padding-left: 5px;">
-			<asp:label id="lblConfirmation" visible="false" runat="server"></asp:label>
-			
-			<span id="Welcome">There are no operator available for the moment.<br />Please leave us a message.</span>
-			<br /><br />
-			<span class="formField">Your Email</span><br />
-			<asp:textbox id="txtSendBy" runat="server"></asp:textbox><br /><br />
-			<span class="formField">Your questions / comments</span><br />
-			<asp:textbox id="txtComment" textmode="MultiLine" width="350" height="75" runat="server"></asp:textbox><br /><br />
-			<asp:linkbutton id="lnkSendEmail" text="Send Email" runat="server" onclick="lnkSendEmail_Click"></asp:linkbutton>
-			</div>
-    </asp:panel>
-    <asp:panel id="pnlRequest" visible="true" runat="server">
-    <div style="padding-left: 5px;">
-    <span id="Welcome">Please fill the form below to request a chat session.</span><br />
-    <span class="formField">Your Name</span><br />
-    <asp:textbox id="txtName" runat="server"></asp:textbox><br /><br />
-    <span class="formField">Your Email</span><br />
-    <asp:textbox id="txtEmail" runat="server"></asp:textbox><br /><br />
-    <asp:linkbutton id="lnkStartChat" text="Request Chat" runat="server" onclick="lnkStartChat_Click"></asp:linkbutton>
-    </div>
-    </asp:panel>
-    <asp:panel id="pnlChat" visible="false" runat="server">
-		<div id="chat">
-		<asp:updatepanel id="upChat" runat="server">
-		<triggers>
-			<asp:asyncpostbacktrigger controlid="timerRefresh" />
-		</triggers>
-		<contenttemplate>
-				<asp:literal id="litChat" runat="server"></asp:literal>
-		</contenttemplate>
-		</asp:updatepanel>
-		</div>
-    <br />
-    &nbsp;&nbsp;
-    <asp:textbox id="txtMsg" width="500" runat="server"></asp:textbox>
-    <asp:button id="btnSendMsg" text="Send" runat="server" onclick="btnSendMsg_Click" /><br /><br />
-		<asp:timer id="timerRefresh" interval="2123" runat="server" ontick="timerRefresh_Tick">
-		</asp:timer>
-    </asp:panel>
-    </form>
+        <asp:ScriptManager ID="ScriptManager1" runat="server" EnablePageMethods="True" >
+            <Scripts>
+                <asp:ScriptReference Path="SendMsg.js" />
+            </Scripts>
+        </asp:ScriptManager>
+        <img src="Images/logo_base.JPG" alt="Itookia logo" /><br />
+        <br />
+        <asp:Panel ID="pnlNoOperator" Visible="false" runat="server">
+            <div style="padding-left: 5px;">
+                <asp:Label ID="lblConfirmation" Visible="false" runat="server"></asp:Label>
+                <span id="Welcome">There are no operator available for the moment.<br />
+                    Please leave us a message.</span>
+                <br />
+                <br />
+                <span class="formField">Your Email</span><br />
+                <asp:TextBox ID="txtSendBy" runat="server"></asp:TextBox><br />
+                <br />
+                <span class="formField">Your questions / comments</span><br />
+                <asp:TextBox ID="txtComment" TextMode="MultiLine" Width="350" Height="75" runat="server"></asp:TextBox><br />
+                <br />
+                <asp:LinkButton ID="lnkSendEmail" Text="Send Email" runat="server" OnClick="lnkSendEmail_Click"></asp:LinkButton>
+            </div>
+        </asp:Panel>
+        <asp:Panel ID="pnlRequest" Visible="true" runat="server">
+            <div style="padding-left: 5px;">
+                <span id="Welcome">Please fill the form below to request a chat session.</span><br />
+                <span class="formField">Your Name</span><br />
+                <asp:TextBox ID="txtName" runat="server"></asp:TextBox><br />
+                <br />
+                <span class="formField">Your Email</span><br />
+                <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox><br />
+                <br />
+                <asp:LinkButton ID="lnkStartChat" Text="Request Chat" runat="server" OnClick="lnkStartChat_Click"></asp:LinkButton>
+            </div>
+        </asp:Panel>
+        <asp:Panel ID="pnlChat" Visible="false" runat="server">
+			<div id="chat"> 
+            <asp:UpdatePanel ID="upChat" runat="server" UpdateMode="Conditional" ChildrenAsTriggers="False">
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="timerRefresh" EventName="Tick" />
+                </Triggers>
+                <ContentTemplate>
+                    
+                        <asp:Literal ID="litChat" runat="server"></asp:Literal>
+            
+                    <asp:Timer ID="timerRefresh" Interval="2123" runat="server" OnTick="timerRefresh_Tick">
+                    </asp:Timer>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+             </div>
+             <br /> 
+            <br />
+            <br />   
+            <input type="text" id="txtMsg" runat="server" style="width:500px;"  onkeypress="checkEnter(event)" />
+            <input type="button" id="btnSend" value="Send" onclick="CallSendMsg()" />
+            </asp:Panel>
+    </form>    
 </body>
 </html>
--- website/Chat.aspx.cs
+++ website/Chat.aspx.cs
@@ -1,3 +1,7 @@
+
+
+
+
 using System;
 using System.Data;
 using System.Configuration;
@@ -10,137 +14,165 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using System.Collections.Generic;
 using System.Net.Mail;
+using System.Web.Services;
+using System.Xml;
+using System.Web.Services.Protocols;
+using System.Web.Script.Services;
 
 public partial class Chat : System.Web.UI.Page
 {
-	public string VisitorName
-	{
-		get
-		{
-			if (ViewState["__visitorName"] != null)
-				return ViewState["__visitorName"].ToString();
-			else
-				return string.Empty;
-		}
-		set { ViewState["__visitorName"] = value; }
-	}
+
+    public string VisitorName
+    {
+        get
+        {
+            if (ViewState["__visitorName"] != null)
+                return ViewState["__visitorName"].ToString();
+            else
+                return string.Empty;
+        }
+        set { ViewState["__visitorName"] = value; }
+    }
+
+    static public string VName
+    {
+
+        get
+        {
+            return HttpContext.Current.Session["VisitorName"].ToString();
+        }
+        set
+        {
+            HttpContext.Current.Session["VisitorName"] = value;
+        }
+    }
 
     protected void Page_Load(object sender, EventArgs e)
     {
-		if (!Page.IsPostBack)
-		{
-			pnlNoOperator.Visible = pnlChat.Visible = pnlRequest.Visible = false;
+        if (!Page.IsPostBack)
+        {
+            pnlNoOperator.Visible = pnlChat.Visible = pnlRequest.Visible = false;
 
-			if (OperatorService.GetOperatorStatus())
-			{
-				pnlRequest.Visible = true;
-			}
-			else
-			{
-				pnlNoOperator.Visible = true;
-			}
-		}
+            if (OperatorService.GetOperatorStatus())
+            {
+                pnlRequest.Visible = true;
+            }
+            else
+            {
+                pnlNoOperator.Visible = true;
+            }
+        }
+
+    }
+    protected void lnkStartChat_Click(object sender, EventArgs e)
+    {
+        // Initiate a chat request
+        string chatId = Guid.NewGuid().ToString();
+        if (Request.Cookies["chatId"] != null)
+        {
+            Response.Cookies["chatId"].Value = chatId;
+        }
+        else
+        {
+            HttpCookie cookie = new HttpCookie("chatId", chatId);
+            Response.Cookies.Add(cookie);
+        }
+
+        if (Request.Cookies[chatId + "_lastId"] != null)
+        {
+            Response.Cookies[chatId + "_lastId"].Value = "0";
+        }
+        else
+        {
+            HttpCookie cookie = new HttpCookie(chatId + "_lastId", "0");
+            Response.Cookies.Add(cookie);
+        }
+
+        ChatRequestInfo request = new ChatRequestInfo();
+        request.AcceptByOpereratorId = 1;
+        request.ChatId = chatId;
+        request.RequestDate = DateTime.Now;
+        request.VisitorEmail = txtEmail.Text;
+        if (Request.UserHostAddress != null)
+            request.VisitorIP = Request.UserHostAddress.ToString();
+        request.VisitorName = txtName.Text;
+        if (Request.ServerVariables["HTTP_USER_AGENT"] != null)
+            request.VisitorUserAgent = Request.ServerVariables["HTTP_USER_AGENT"].ToString();
+        request.WasAccept = false;
+
+        ChatService.RequestChat(request);
+
+        ChatMessageInfo msg = new ChatMessageInfo(1, request.ChatId, string.Empty, DateTime.Now, "Waiting for an operator to accept your request.");
+        ChatService.AddMessage(msg);
+
+        // we set the visitor name in the ViewState
+        VisitorName = request.VisitorName;
+        VName = request.VisitorName;
+
+        pnlChat.Visible = true;
+        pnlRequest.Visible = false;
     }
 
-	protected void lnkStartChat_Click(object sender, EventArgs e)
-	{
-		// Initiate a chat request
-		string chatId = Guid.NewGuid().ToString();
-		if (Request.Cookies["chatId"] != null)
-		{
-			Response.Cookies["chatId"].Value = chatId;
-		}
-		else
-		{
-			HttpCookie cookie = new HttpCookie("chatId", chatId);
-			Response.Cookies.Add(cookie);
-		}
+    protected void timerRefresh_Tick(object sender, EventArgs e)
+    {
+        if (Request.Cookies["chatId"] != null)
+        {
+            string chatId = Request.Cookies["chatId"].Value.ToString();
+            if (Request.Cookies[chatId + "_lastId"] != null)
+            {
+                int lastId = int.Parse(Request.Cookies[chatId + "_lastId"].Value.ToString());
+                List<ChatMessageInfo> messages = ChatService.GetMessages(chatId, lastId);
+                if (messages.Count > 0)
+                {
+                    for (int i = messages.Count - 1; i >= 0; i--)
+                    {
+                        litChat.Text += string.Format("<span class=\"chatName\">{0} :</span>{1}<br />", messages[i].Name, messages[i].Message);
+                    }
 
-		if (Request.Cookies[chatId + "_lastId"] != null)
-		{
-			Response.Cookies[chatId + "_lastId"].Value = "0";
-		}
-		else
-		{
-			HttpCookie cookie = new HttpCookie(chatId + "_lastId", "0");
-			Response.Cookies.Add(cookie);
-		}
+                    lastId = messages[0].MessageId;
 
-		ChatRequestInfo request = new ChatRequestInfo();
-		request.AcceptByOpereratorId = 1;
-		request.ChatId = chatId;
-		request.RequestDate = DateTime.Now;
-		request.VisitorEmail = txtEmail.Text;
-		if (Request.UserHostAddress != null)
-			request.VisitorIP = Request.UserHostAddress.ToString();
-		request.VisitorName = txtName.Text;
-		if( Request.ServerVariables["HTTP_USER_AGENT"] != null )
-			request.VisitorUserAgent = Request.ServerVariables["HTTP_USER_AGENT"].ToString();
-		request.WasAccept = false;
+                    // set the lastId
+                    Response.Cookies[chatId + "_lastId"].Value = lastId.ToString();
 
-		ChatService.RequestChat(request);
+                }
+            }
+        }
+    }
 
-		ChatMessageInfo msg = new ChatMessageInfo(1, request.ChatId, string.Empty, DateTime.Now, "Waiting for an operator to accept your request.");
-		ChatService.AddMessage(msg);
+    [System.Web.Services.WebMethod]
+    [ScriptMethod(UseHttpGet = true)]
+    public static string SendMsg(string msg, string chtID)
+    {
+        if (chtID != null && msg.Length > 0)
+        {
+            // Add a new message to the discussion
+            string chatId = chtID;
 
-		// we set the visitor name in the ViewState
-		VisitorName = request.VisitorName;
+            // we get the last MessageId
+            int id = ChatService.GetLastMessageId(chatId) + 1;
 
-		pnlChat.Visible = true;
-		pnlRequest.Visible = false;
-	}
-	protected void timerRefresh_Tick(object sender, EventArgs e)
-	{
-		if (Request.Cookies["chatId"] != null)
-		{
-			string chatId = Request.Cookies["chatId"].Value.ToString();
-			if (Request.Cookies[chatId + "_lastId"] != null)
-			{
-				int lastId = int.Parse(Request.Cookies[chatId + "_lastId"].Value.ToString());
-				List<ChatMessageInfo> messages = ChatService.GetMessages(chatId, lastId);
-				if (messages.Count > 0)
-				{
-					for (int i = messages.Count - 1; i >= 0; i--)
-					{
-						litChat.Text += string.Format("<span class=\"chatName\">{0} :</span>{1}<br />", messages[i].Name, messages[i].Message);
-					}
+            ChatMessageInfo mesg = new ChatMessageInfo(id, chatId, VName, DateTime.Now, msg);
+            ChatService.AddMessage(mesg);
+        }
+        return "";
+    }
 
-					lastId = messages[0].MessageId;
-					
-					// set the lastId
-					Response.Cookies[chatId + "_lastId"].Value = lastId.ToString();
-				}
-			}
-		}
-	}
-	protected void btnSendMsg_Click(object sender, EventArgs e)
-	{
-		if (Request.Cookies["chatId"] != null && txtMsg.Text.Length > 0)
-		{
-			// Add a new message to the discussion
-			string chatId = Request.Cookies["chatId"].Value.ToString();
-			
-			// we get the last MessageId
-			int id = ChatService.GetLastMessageId(chatId) + 1;
 
-			ChatMessageInfo msg = new ChatMessageInfo(id, chatId, VisitorName, DateTime.Now, txtMsg.Text);
-			ChatService.AddMessage(msg);
-			txtMsg.Text = string.Empty;
-		}
-	}
-	protected void lnkSendEmail_Click(object sender, EventArgs e)
-	{
-		// we send an email to the configured email on the web.config
-		MailMessage mail = new MailMessage();
-		mail.From = new MailAddress(txtSendBy.Text);
-		mail.To.Add(new MailAddress(ConfigurationManager.AppSettings["Email"].ToString()));
-		mail.Subject = "Message from your LiveChat application";
-		mail.Body = txtComment.Text;
 
-		SmtpClient mailer = new SmtpClient(ConfigurationManager.AppSettings["SMTPServer"].ToString());
-		mailer.Send(mail);
+    protected void lnkSendEmail_Click(object sender, EventArgs e)
+    {
+        // we send an email to the configured email on the web.config
+        MailMessage mail = new MailMessage();
+        mail.From = new MailAddress(txtSendBy.Text);
+        mail.To.Add(new MailAddress(ConfigurationManager.AppSettings["Email"].ToString()));
+        mail.Subject = "Message from your LiveChat application";
+        mail.Body = txtComment.Text;
 
-		lblConfirmation.Visible = true;
-		lblConfirmation.Text = "Thank you, we will answer your email as soon as possible.";
-	}
+        SmtpClient mailer = new SmtpClient(ConfigurationManager.AppSettings["SMTPServer"].ToString());
+        mailer.Send(mail);
+
+        lblConfirmation.Visible = true;
+        lblConfirmation.Text = "Thank you, we will answer your email as soon as possible.";
+    }
+
 }
