15dde7f38f6286a12da35daed2633ac4d12f453d
Dominic St-Pierre <dominicstpierre@gmail.com>
2010-12-19 05:55:34 -0500

    Added nuget/nuspec files

---
+++ src/content/Areas/LiveChat/LiveChatAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace LCSK.Web.Areas.LiveChat
+{
+	public class LiveChatAreaRegistration : AreaRegistration
+	{
+		public override string AreaName
+		{
+			get
+			{
+				return "LiveChat";
+			}
+		}
+
+		public override void RegisterArea(AreaRegistrationContext context)
+		{
+			context.MapRoute(
+				"LiveChat_default",
+				"LiveChat/{action}/{id}",
+				new { controller = "Chat", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
+}
