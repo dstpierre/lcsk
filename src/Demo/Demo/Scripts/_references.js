7afff6fd750291e727b8d72459d9a641b603445d
Dominic St-Pierre <dominicstpierre@gmail.com>
2016-11-20 09:08:14 -0500

    starting point for Parle

--- src/Demo/Demo/Scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="jquery-2.1.1.js" />
-/// <reference path="jquery-ui-1.8.20.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.1.0.debug.js" />
-/// <reference path="modernizr-2.7.2.js" />
